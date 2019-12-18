378ca0af2b6e3e672729d9fabbfe55f9f5eb1f33
williambasso <50145828+williambasso@users.noreply.github.com>
2019-12-18 11:28:57 -0300

    Update produtos_dao.py

--- revisaoEX2/dao/produtos_dao.py
+++ revisaoEX2/dao/produtos_dao.py
@@ -39,7 +39,14 @@ class ProdutosDao(BaseDao):
         )"""
         return super().inserir(comando_sql_insert)
     
+    def listar(self, tipo):
+        comando_sql_listar = f"""SELECT produtos.produto, produtos.preco, tipo_produto.tipo FROM produtos JOIN tipo_produto
+        ON produtos.fk_tipo = tipo_produto.id WHERE fk_tipo = {tipo}
+        """
+        super().listar(comando_sql_listar)
+
     def has_nome(self,produto):
         comando_sql_buscar_nome = f"SELECT * from produtos where produto like '%{produto}%' limit 1 "
         a = super().buscar_por_id(comando_sql_buscar_nome)
         return(True if a else False)
+
